6c8f1f5f9ea9e6ef4489d3d6fb2f2c11241369be
Hammoud Taha <thismyhamoud@gmail.com>
2024-09-26 16:59:01 +0300

    creating Hive local database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hammoud/database/sqflite_db.dart';
+import 'package:hammoud/view/noteapp_page.dart';
 
 void main() {
-  //debugPaintSizeEnabled = true;
-
   runApp(const MyApp());
 }
 
@@ -12,67 +11,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MyDataBase databse = MyDataBase();
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: const Text("SQFLITE"),
-        ),
-        body: ListView(
-          children: [
-            MaterialButton(
-              onPressed: () async {
-                List<Map> list =
-                    await databse.readInformation("SELECT * FROM 'user'");
-
-                print(list);
-              },
-              color: Colors.red,
-              child: const Text(
-                'READ',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () async {
-                List list =
-                    await databse.insertInformation('User', {'name': 'pppppp'});
-                print(list);
-              },
-              color: Colors.red,
-              child: const Text(
-                'INSERT',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () async {
-                int response = await databse.updataInformation(
-                    "UPDATE User SET name = 'Ali' WHERE id = 3");
-                print(response);
-              },
-              color: Colors.red,
-              child: const Text(
-                'UPDAE',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            MaterialButton(
-              onPressed: () async {
-                int response = await databse
-                    .deleteInformation("DELETE FROM User WHERE id = 1");
-                print(response);
-              },
-              color: Colors.red,
-              child: const Text(
-                'DELETE',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const NotesApp(),
     );
   }
 }
+
+
+// Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.amber,
+//           title: const Text("SQFLITE"),
+//         ),
+//         body: ListView(
+//           children: [
+//             MaterialButton(
+//               onPressed: () async {
+//                 List<Map> list =
+//                     await databse.readInformation("SELECT * FROM 'user'");
+
+//                 print(list);
+//               },
+//               color: Colors.red,
+//               child: const Text(
+//                 'READ',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//             MaterialButton(
+//               onPressed: () async {
+//                 List list =
+//                     await databse.insertInformation('User', {'name': 'pppppp'});
+//                 print(list);
+//               },
+//               color: Colors.red,
+//               child: const Text(
+//                 'INSERT',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//             MaterialButton(
+//               onPressed: () async {
+//                 int response = await databse.updataInformation(
+//                     "UPDATE User SET name = 'Ali' WHERE id = 3");
+//                 print(response);
+//               },
+//               color: Colors.red,
+//               child: const Text(
+//                 'UPDAE',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//             MaterialButton(
+//               onPressed: () async {
+//                 int response = await databse
+//                     .deleteInformation("DELETE FROM User WHERE id = 1");
+//                 print(response);
+//               },
+//               color: Colors.red,
+//               child: const Text(
+//                 'DELETE',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+    
